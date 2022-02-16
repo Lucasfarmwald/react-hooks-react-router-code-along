98996183eac894ec4bf066fdab44619eb138bcc2
lucasfarmwald <farmwaldlucas@gmail.com>
2022-02-15 18:21:01 -0600

    added about and login components

--- src/index.js
+++ src/index.js
@@ -10,6 +10,33 @@ function Home() {
   );
 }
 
+function About() {
+  return(
+    <div>
+      <h1>
+        this is my About component!
+      </h1>
+    </div>
+  );
+}
+
+function Login() {
+  return(
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="username"/>
+        </div>
+        <div>
+          <input tpye="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit"/>
+      </form>
+    </div>
+  )
+}
+
 function App() {
   return (
     <Route path="/">
